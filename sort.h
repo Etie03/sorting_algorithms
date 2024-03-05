4f2f4219213d7dcab6f17762d69c8e62fbdea723
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2024-03-05 06:43:49 -0800

    header

--- sort.h
+++ sort.h
@@ -29,6 +29,7 @@ void quick_sort(int *array, size_t size);
 int lom_partition(int *array, int low, int high, size_t size);
 void lomuto_quicksort(int *array, int low, int high, size_t size);
 void swap_node(listint_t **list, listint_t *node);
+void merger_sort(int *array, int *temp, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
